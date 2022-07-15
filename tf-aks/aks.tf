88c3441b987723ca8ef5a6e8312e9c03864b07d9
host1812 <host1812@gmail.com>
2022-07-15 16:00:34 -0700

    simple terraform to create aks

---
+++ tf-aks/aks.tf
@@ -0,0 +1,36 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-aks"
+  location = "East Us"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "aks-us-east-1a"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  dns_prefix          = "aks-us-east-1a"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Dev"
+  }
+}
+
+output "client_certificate" {
+  value     = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
+  sensitive = true
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.example.kube_config_raw
+
+  sensitive = true
+}
