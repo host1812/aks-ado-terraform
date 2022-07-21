5f510378edff04c049649a7ee4b90b8f600dfd41
host1812 <host1812@gmail.com>
2022-07-20 17:05:54 -0700

    add dev vm

---
+++ terraform/vm.tf
@@ -0,0 +1,27 @@
+resource "azurerm_linux_virtual_machine" "af-vm" {
+  name                = "af-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.af-nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/host1812_azure_vms.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "20.04-LTS"
+    version   = "latest"
+  }
+}
