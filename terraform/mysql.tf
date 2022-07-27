197328a59704f003208493269eebc3b3232ac532
host1812 <host1812@gmail.com>
2022-07-27 15:49:48 -0700

    update mysql definitions

--- terraform/mysql.tf
+++ terraform/mysql.tf
@@ -3,7 +3,7 @@ resource "azurerm_mysql_server" "mysql" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
-  administrator_login          = "root"
+  administrator_login          = "host1812"
   administrator_login_password = "testtest1!"
 
   sku_name   = "B_Gen5_1"
@@ -15,6 +15,6 @@ resource "azurerm_mysql_server" "mysql" {
   geo_redundant_backup_enabled      = false
   infrastructure_encryption_enabled = false
   public_network_access_enabled     = true
-  ssl_enforcement_enabled           = true
-  ssl_minimal_tls_version_enforced  = "TLS1_2"
+  ssl_enforcement_enabled           = false
+  ssl_minimal_tls_version_enforced  = "TLSEnforcementDisabled"
 }
