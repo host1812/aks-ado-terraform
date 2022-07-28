62a71ebca8ba86e3fddd69d4c91855d681fb8c58
host1812 <host1812@gmail.com>
2022-07-28 16:12:50 -0700

    add new pip for aks ingress

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,7 @@
 output "vm_public_ip_address" {
   value = azurerm_public_ip.af-vm-pip.ip_address
 }
+
+output "aks_ingress_public_ip_address" {
+  value = azurerm_public_ip.af-aks-ingress-pip.ip_address
+}
--- terraform/pip.tf
+++ terraform/pip.tf
@@ -4,3 +4,10 @@ resource "azurerm_public_ip" "af-vm-pip" {
   location            = azurerm_resource_group.rg.location
   allocation_method   = "Static"
 }
+
+resource "azurerm_public_ip" "af-aks-ingress-pip" {
+  name                = "af-aks-ingress-pip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+}
